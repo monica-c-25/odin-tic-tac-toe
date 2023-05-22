4aaf874f2e7b30fb781e23002b4d44bb003ca9a3
Monica Cao <116030921+monica-c-25@users.noreply.github.com>
2023-05-22 11:00:50 -0700

    Set initial variables

--- javascript.js
+++ javascript.js
@@ -31,10 +31,26 @@ const Gameboard = (() => {
 //displayController module
 const displayController = (() => {
 
+    //create players
+    const playerOne = player('one', 'X')
+    const playerTwo = player('two', 'O')
+
+    //initial variables 
+    let currentPlayer = playerOne;
+    let finalWinner = false;
+    let spots = 9;
+
+    //winning combinations
+    const winningCombos = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
 })();
 
-
-
-const playerOne = player('one', 'X')
-const playerTwo = player('two', 'O')
 
