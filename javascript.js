8f1d5773483fc7538a4c5299f36b0b014e40d3e3
Monica Cao <116030921+monica-c-25@users.noreply.github.com>
2023-05-22 15:45:41 -0700

    Create functions to alternateturns and declare winner

--- javascript.js
+++ javascript.js
@@ -25,17 +25,29 @@ const gameBoard = (() => {
             board[index] = displayController.currentPlayer.mark;
 
             //deactivate box to prevent further marks 
-            box.pointerEvents = 'none'
+            box.style.pointerEvents = 'none';
 
             //update spots 
             displayController.spots -= 1;
 
             //check if there is a winner 
             displayController.winner();
+
+            //if there is no winner yet
+            if (displayController.finalWinner === true) {
+                document.getElementById('subtext').innerHTML = `${displayController.currentPlayer.name} is the Winner`; 
+                } else {
+                    (displayController.finalWinner === false) 
+                        if (displayController.spots > 0) {
+                        displayController.nextPlayer();
+                            } else {
+                                displayController.tie();
+                            }
+            }
+
         })
     }
     )
-
     return {
         board
     }
@@ -47,8 +59,8 @@ const gameBoard = (() => {
 const displayController = (() => {
 
     //create players
-    const playerOne = player('one', 'X')
-    const playerTwo = player('two', 'O')
+    const playerOne = player('Player 1', 'X')
+    const playerTwo = player('Player 2', 'O')
 
     //initial variables 
     let currentPlayer = playerOne;
@@ -69,15 +81,37 @@ const displayController = (() => {
 
     function winner() {
         winningCombos.forEach((combo, index) => {
-            if (gameBoard.board[combo[0]] === this.currentPlayer.mark && combo[[1]] === this.currentPlayer.mark && combo[[2]] === this.currentPlayer.mark) {
-                finalWinner = true;
-            }
+            if ((gameBoard.board[combo[0]] === displayController.currentPlayer.mark) && (gameBoard.board[combo[1]] === displayController.currentPlayer.mark) && (gameBoard.board[combo[2]] === displayController.currentPlayer.mark)) {
+                Array.from(document.getElementsByClassName('box')).forEach((box) => {
+                    box.style.pointerEvents = 'none'
+                });
+                displayController.finalWinner = true;               
+            } 
         })
+    }
+
+    function nextPlayer() {
+        if (displayController.currentPlayer === playerOne) {
+            displayController.currentPlayer = playerTwo;
+            document.getElementById('subtext').innerHTML = "Player 2's Turn"
+        } else {
+            displayController.currentPlayer = playerOne;
+            displayController.subtext = "Player Two's Turn";
+            document.getElementById('subtext').innerHTML = "Player 1's Turn"
+        }
+    }
+
+    function tie() {
+        document.getElementById('subtext').innerHTML = "Nobody Wins! It's a Tie."
     }
 
     return {
         currentPlayer,
+        finalWinner,
         spots,
+        winner,
+        nextPlayer,
+        tie,
     }
 })();
 
